aaa3fc3fe1b6f7da2087e69531a0e0d6ca2feb22
Khaledtello <khaldtllw@gmail.com>
2026-06-24 12:02:02 +0300

    make simple collision with wall

--- src/physics/systems/collisions.js
+++ src/physics/systems/collisions.js
@@ -4,23 +4,28 @@ export function resolveTableCollisions(ball) {
   const r = ball.radius;
   const e = PHYSICS.railRestitution;
 
-  if (ball.position.x + r > TABLE.maxX) {
-    ball.position.x = TABLE.maxX - r;
+  const maxX = TABLE.width / 2;
+  const minX = -TABLE.width / 2;
+  const maxY = TABLE.height / 2;
+  const minY = -TABLE.height / 2;
+
+  if (ball.position.x + r > maxX) {
+    ball.position.x = maxX - r;
     ball.velocity.x *= -e;
   }
 
-  if (ball.position.x - r < TABLE.minX) {
-    ball.position.x = TABLE.minX + r;
+  if (ball.position.x - r < minX) {
+    ball.position.x = minX + r;
     ball.velocity.x *= -e;
   }
 
-  if (ball.position.y + r > TABLE.maxY) {
-    ball.position.y = TABLE.maxY - r;
+  if (ball.position.y + r > maxY) {
+    ball.position.y = maxY - r;
     ball.velocity.y *= -e;
   }
 
-  if (ball.position.y - r < TABLE.minY) {
-    ball.position.y = TABLE.minY + r;
+  if (ball.position.y - r < minY) {
+    ball.position.y = minY + r;
     ball.velocity.y *= -e;
   }
 }
